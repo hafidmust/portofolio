cccfdff735196892f75d1c10029ab3579e1522c3
hafidmust <hafidalimustaqim13@gmail.com>
2021-07-06 21:58:16 +0700

    add object

---
+++ js/data_project.js
@@ -0,0 +1,23 @@
+let data_project = {
+    0: {
+        title : "NGII Kamus",
+        desc : "Some example text some example text. John Doe is an architect and engineer",
+        image : "fa-language"
+    },
+    1:{
+        title : "Siaga Bencana",
+        desc : "Some example text some example text. John Doe is an architect and engineer",
+        image : "fa-house-damage"
+    },
+    2:{
+        title : "Foody",
+        desc : "Some example text some example text. John Doe is an architect and engineer",
+        image : "fa-pizza-slice"
+
+    },
+    3:{
+        title : "Movie App",
+        desc : "Some example text some example text. John Doe is an architect and engineer",
+        image : "fa-film"
+    }
+}
